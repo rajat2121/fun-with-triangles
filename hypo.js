3fd25717498a359936dab5c1ab36cc1ed80d52e1
rajat2121 <rajatawasthi99@gmail.com>
2021-12-05 14:00:22 +0530

    hypo.js
    calculate hypo

---
+++ hypo.js
@@ -0,0 +1,36 @@
+s (26 sloc)  922 Bytes
+   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>calcuulate hypoteneuse</title>
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <nav class="nav">
+        <a class="link" href="Index.html">Triangle</a>
+        <a class="link link-active " href="hypo.html">Hypoteneuse</a>
+        <a class=" link" href="quiz.html">Quiz</a>
+        <a class="link" href="areaOfTriangle.html">AreaOfTriangle</a>
+
+    </nav>
+
+
+    <h1>Calculate Hypoteneuse</h1>
+    <label>Length a</label>
+    <input type="number" class="input-side"/>
+    <label>length b</label>
+    <input type="number" class="input-side"/>
+
+    <button id="calculate-Btn">Calculate</button>
+
+    <div id="output"></div>
+
+    <script src="./hypo.js"></script>
+    
+</body>
+</html>
